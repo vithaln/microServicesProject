13825b535f1192477c3c2982556a75b2f4b8a349
vithaln <vnnivargi1998@gmail.com>
2022-12-12 22:11:36 +0530

    inital commit fo API-Gateway

--- UserService/src/main/java/com/code/vithal/extranalConfig/RatingService.java
+++ UserService/src/main/java/com/code/vithal/extranalConfig/RatingService.java
@@ -1,15 +1,36 @@
 package com.code.vithal.extranalConfig;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.code.vithal.entity.Rating;
 
+@Service
 @FeignClient(name = "RATING-SERVICE")
 public interface RatingService {
 
 	
-	@GetMapping("/ratings/user/{ratingId}")
-	Rating getRatings(@PathVariable String ratingId);
+	/*
+	 * @GetMapping("/ratings/user/{ratingId}") Rating getRatings(@PathVariable
+	 * String ratingId);
+	 */
+	
+	//create ratings
+	
+	@PostMapping("/ratings")
+	public Rating createRatings(Rating values);
+	
+	//update ratings
+	
+	@PutMapping("/ratings/{ratingId}")
+	public Rating updateRatings(@PathVariable String ratingId,Rating rating);
+	
+	//delete ratings
+	
+	@DeleteMapping("/ratings/{ratingId}")
+	public void deleteRatings(String ratingId);
 }
--- UserService/src/test/java/com/example/demo/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/example/demo/UserServiceApplicationTests.java
@@ -6,8 +6,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UserServiceApplicationTests {
 
+	/*
+	 * @Autowired private RatingService ratingService;
+	 */
 	@Test
 	void contextLoads() {
 	}
 
+	/*
+	 * @Test void createRatings() {
+	 * 
+	 * Rating rating = Rating.builder().rating(10).userId("").hotelId("").
+	 * feedback("rating created by using feignClient").build(); Rating savedRatings
+	 * = ratingService.createRatings(rating);
+	 * 
+	 * System.out.println("===created ratings by using feignClient...===="); }
+	 */
 }
